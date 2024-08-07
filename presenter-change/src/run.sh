8c0981197243ad6dea144e9615a4183efe7ce05d
Andre Kutianski <andre@eunarede.com>
2024-08-07 16:28:16 +0000

    refactor: Improve handling of file

--- presenter-change/src/run.sh
+++ presenter-change/src/run.sh
@@ -26,6 +26,13 @@ if ! command -v bbb-conf > /dev/null; then
     exit 1
 fi
 
+# Define a temporary path to store the downloaded PDF
+# to avoid collisions, define a unique name for the temp file
+temp_pdf="/tmp/$(date +%s)_bbb_downloaded_presentation.pdf"
+#temp_pdf="/tmp/bbb_downloaded_presentation.pdf"
+# Replace the document's name with the default name used by BigBlueButton
+presentation_path="/var/www/bigbluebutton-default/assets"
+pdf_name="default.pdf"
 
 # Prompt the user to enter the path to the new PDF document
 read -r -p "Enter the path to the new PDF document: " pdf_path
@@ -37,8 +44,6 @@ echo "Entered PDF path: $pdf_path"
 #if [[ "$pdf_path" =~ ^(https?:\/\/)[\w.-]+(\.[\w]+)+([\w.,@?^=%&amp;\/:+#]*) ]]; then
 if [[ "$pdf_path" == http* ]]; then
     echo "Downloading PDF from remote location..."
-    # Define a temporary path to store the downloaded PDF
-    temp_pdf="/tmp/downloaded_presentation.pdf"
     # Use wget or curl to download the PDF document
     wget -O "$temp_pdf" "$pdf_path" || curl -o "$temp_pdf" "$pdf_path"
     # Check if the download was successful
@@ -54,18 +59,16 @@ else
         echo "Error: The specified PDF file does not exist."
         exit 1
     fi
+    cp "$pdf_path" "$temp_pdf"
 fi
 
 # Check if the document is a PDF
-if ! file -b --mime-type "$pdf_path" | grep -q "application/pdf"; then
+if ! file -b --mime-type "$temp_pdf" | grep -q "application/pdf"; then
     echo "Error: The specified document is not a PDF."
     exit 1
 fi
 
-# Replace the document's name with the default name used by BigBlueButton
-presentation_path="/var/www/bigbluebutton-default/assets"
-pdf_name="default.pdf"
 
-cp "$pdf_path" "$presentation_path/$pdf_name"
+cp "$temp_pdf" "$presentation_path/$pdf_name"
 
 echo "The default BigBlueButton PDF presentation has been successfully changed to $pdf_path."
